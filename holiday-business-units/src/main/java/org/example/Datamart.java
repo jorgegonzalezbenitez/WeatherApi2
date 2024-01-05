cbc989e1595851c330b7301586c09855b0fcf6bf
first commit <jorgeglezbenitez@gmail.com>
2024-01-05 22:39:28 +0000

    datamart corrected

--- holiday-business-units/src/main/java/org/example/Datamart.java
+++ holiday-business-units/src/main/java/org/example/Datamart.java
@@ -6,6 +6,10 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.sql.*;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class Datamart implements Listener{
 
@@ -27,26 +31,47 @@ public class Datamart implements Listener{
             String lat = location.getAsJsonPrimitive("lat").getAsString();
             String lon = location.getAsJsonPrimitive("lon").getAsString();
 
+            LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.parse(ts), ZoneId.systemDefault());
+            String ts1 = dateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd/HH:mm"));
+
             try (Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\cgsos\\Downloads\\DataBase\\weather.db")) {
-                String tableName = name.toLowerCase().replace(" ", "_");
-                String createTableSQL = "CREATE TABLE IF NOT EXISTS " + tableName + " (" +
-                        "cloud TEXT," +
-                        "windSpeed TEXT," +
-                        "temp TEXT," +
-                        "humidity TEXT," +
-                        "instant TEXT," +
-                        "propRain TEXT," +
-                        "ts TEXT," +
-                        "ss TEXT," +
-                        "lat TEXT," +
-                        "lon TEXT" +
-                        ")";
+                String tableName = name.toLowerCase().replaceAll(" ", "_");
+                try {
+                    String createTableSQL = "CREATE TABLE IF NOT EXISTS " + tableName + " (" +
+                            "cloud TEXT," +
+                            "windSpeed TEXT," +
+                            "temp TEXT," +
+                            "humidity TEXT," +
+                            "instant TEXT," +
+                            "propRain TEXT," +
+                            "ts TEXT," +
+                            "ss TEXT," +
+                            "lat TEXT," +
+                            "lon TEXT" +
+                            ")";
 
-                try (Statement statement = connection.createStatement()) {
-                    statement.executeUpdate(createTableSQL);
+                    try (Statement statement = connection.createStatement()) {
+                        statement.executeUpdate(createTableSQL);
+                        System.out.println("Created table for: " + tableName);
+                    }
+                } catch (SQLException exc) {
+                    exc.printStackTrace();
+                }
 
-                    String insertWeatherSQL = "INSERT INTO " + tableName + " ( cloud, windSpeed, temp, humidity, instant, propRain, ts, ss, lat, lon) " +
-                            "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                // Iniciar una transacción
+                connection.setAutoCommit(false);
+
+                try {
+                    // Eliminar filas con ts diferente al nuevo ts
+                    String deleteRowsSQL = "DELETE FROM " + tableName + " WHERE ts <> ?";
+                    try (PreparedStatement deleteRowsStatement = connection.prepareStatement(deleteRowsSQL)) {
+                        deleteRowsStatement.setString(1, ts1);
+                        deleteRowsStatement.executeUpdate();
+                    }
+
+                    // Inserción de nuevos datos
+                    String insertWeatherSQL = "INSERT INTO " + tableName + " (cloud, windSpeed, temp, humidity, instant, propRain, ts, ss, lat, lon) " +
+                            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
                     try (PreparedStatement insertStatement = connection.prepareStatement(insertWeatherSQL)) {
                         insertStatement.setString(1, cloud);
                         insertStatement.setString(2, windSpeed);
@@ -54,75 +79,26 @@ public class Datamart implements Listener{
                         insertStatement.setString(4, humidity);
                         insertStatement.setString(5, instant);
                         insertStatement.setString(6, propRain);
-                        insertStatement.setString(7, ts);
+                        insertStatement.setString(7, ts1);
                         insertStatement.setString(8, ss);
                         insertStatement.setString(9, lat);
                         insertStatement.setString(10, lon);
 
                         insertStatement.executeUpdate();
                     }
+
+                    // Confirmar la transacción
+                    connection.commit();
+                    System.out.println("Weather database created");
+                } catch (SQLException exc) {
+                    // Si ocurre un error, hacer rollback de la transacción
+                    connection.rollback();
+                    exc.printStackTrace();
+                } finally {
+                    // Restaurar la configuración de autocommit
+                    connection.setAutoCommit(true);
+                    connection.close();
                 }
-
-            } catch (SQLException | NullPointerException exc) {
-                exc.printStackTrace();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }System.out.println("Weather database created");
-    }
-
-    @Override
-    public void hotelStore(String message) {
-        try {
-            JsonObject jsonObject = new Gson().fromJson(message, JsonObject.class);
-            JsonArray ratesArray = jsonObject.getAsJsonArray("rates");
-            JsonObject dateObject = jsonObject.getAsJsonObject("date");
-            String country = dateObject.getAsJsonPrimitive("country").getAsString();
-
-            try (Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\cgsos\\Downloads\\DataBase\\hotel.db")) {
-                String tableName = country.toLowerCase().replace(" ", "_");
-                String createTableSQL = "CREATE TABLE IF NOT EXISTS " + tableName + " (" +
-                        "name TEXT," +
-                        "hotelKey TEXT," +
-                        "checkIn TEXT," +
-                        "checkOut TEXT," +
-                        "ss TEXT," +
-                        "ts TEXT," +
-                        "code TEXT," +
-                        "rateName TEXT," +
-                        "rate INTEGER," +
-                        "tax INTEGER" +
-                        ")";
-
-                try (Statement statement = connection.createStatement()) {
-                    statement.executeUpdate(createTableSQL);
-
-                    for (JsonElement rateElement : ratesArray) {
-                        JsonObject rateObject = rateElement.getAsJsonObject();
-                        String code = rateObject.getAsJsonPrimitive("code").getAsString();
-                        String rateName = rateObject.getAsJsonPrimitive("rateName").getAsString();
-                        String rate = rateObject.getAsJsonPrimitive("rate").getAsString();
-                        String tax = rateObject.getAsJsonPrimitive("tax").getAsString();
-
-                        String insertRateSQL = "INSERT INTO " + tableName + " (name, hotelKey, checkIn, checkOut, ss, ts, code, rateName, rate, tax) " +
-                                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                        try (PreparedStatement insertStatement = connection.prepareStatement(insertRateSQL)) {
-                            insertStatement.setString(1, dateObject.getAsJsonPrimitive("name").getAsString());
-                            insertStatement.setString(2, dateObject.getAsJsonPrimitive("hotelKey").getAsString());
-                            insertStatement.setString(3, dateObject.getAsJsonPrimitive("checkIn").getAsString());
-                            insertStatement.setString(4, dateObject.getAsJsonPrimitive("checkOut").getAsString());
-                            insertStatement.setString(5, jsonObject.getAsJsonPrimitive("ss").getAsString());
-                            insertStatement.setString(6, jsonObject.getAsJsonPrimitive("ts").getAsString());
-                            insertStatement.setString(7, code);
-                            insertStatement.setString(8, rateName);
-                            insertStatement.setString(9, rate);
-                            insertStatement.setString(10, tax);
-
-                            insertStatement.executeUpdate();
-                        }
-                    }
-                }
-                System.out.println("Hotel database created");
             } catch (SQLException | NullPointerException exc) {
                 exc.printStackTrace();
             }
@@ -130,5 +106,103 @@ public class Datamart implements Listener{
             e.printStackTrace();
         }
     }
+
+
+        @Override
+        public void hotelStore(String message) {
+            try {
+                JsonObject jsonObject = new Gson().fromJson(message, JsonObject.class);
+                JsonArray ratesArray = jsonObject.getAsJsonArray("rates");
+                JsonObject dateObject = jsonObject.getAsJsonObject("date");
+                String country = dateObject.getAsJsonPrimitive("country").getAsString();
+                String name = dateObject.getAsJsonPrimitive("name").getAsString();
+                String hotelKey = dateObject.getAsJsonPrimitive("hotelKey").getAsString();
+                String checkIn = dateObject.getAsJsonPrimitive("checkIn").getAsString();
+                String checkOut = dateObject.getAsJsonPrimitive("checkOut").getAsString();
+
+
+                String ts = jsonObject.getAsJsonPrimitive("ts").getAsString();
+                String ss = jsonObject.getAsJsonPrimitive("ss").getAsString();
+
+                LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.parse(ts), ZoneId.systemDefault());
+                String ts1 = dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd/HH:mm"));
+
+                try (Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\cgsos\\Downloads\\DataBase\\hotel.db")) {
+                    String tableName = country.toLowerCase().replace(" ", "_");
+                    try {
+                        String createTableSQL = "CREATE TABLE IF NOT EXISTS " + tableName + " (" +
+                                "name TEXT," +
+                                "hotelKey TEXT," +
+                                "checkIn TEXT," +
+                                "checkOut TEXT," +
+                                "ss TEXT," +
+                                "ts TEXT," +
+                                "code TEXT," +
+                                "rateName TEXT," +
+                                "rate TEXT," +
+                                "tax TEXT" +
+                                ")";
+
+                        try (Statement statement = connection.createStatement()) {
+                            statement.executeUpdate(createTableSQL);
+                            System.out.println("Created table for: " + tableName);
+                        }
+                    } catch (SQLException exc) {
+                        exc.printStackTrace();
+                    }
+
+
+                    connection.setAutoCommit(false);
+
+                    try {
+                        // Eliminar filas con ts diferente al nuevo ts
+                        String deleteRowsSQL = "DELETE FROM " + tableName + " WHERE ts <> ?";
+                        try (PreparedStatement deleteRowsStatement = connection.prepareStatement(deleteRowsSQL)) {
+                            deleteRowsStatement.setString(1, ts1);
+                            deleteRowsStatement.executeUpdate();
+                        }
+
+                        // Insertar nuevos datos
+                        for (JsonElement rateElement : ratesArray) {
+                            JsonObject rateObject = rateElement.getAsJsonObject();
+                            String code = rateObject.getAsJsonPrimitive("code").getAsString();
+                            String rateName = rateObject.getAsJsonPrimitive("rateName").getAsString();
+                            String rate = rateObject.getAsJsonPrimitive("rate").getAsString();
+                            String tax = rateObject.getAsJsonPrimitive("tax").getAsString();
+
+                            String insertRateSQL = "INSERT INTO " + tableName + " (name, hotelKey, checkIn, checkOut, ss, ts, code, rateName, rate, tax) " +
+                                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                            try (PreparedStatement insertStatement = connection.prepareStatement(insertRateSQL)) {
+                                insertStatement.setString(1, name);
+                                insertStatement.setString(2, hotelKey);
+                                insertStatement.setString(3, checkIn);
+                                insertStatement.setString(4, checkOut);
+                                insertStatement.setString(5, ss);
+                                insertStatement.setString(6, ts1);
+                                insertStatement.setString(7, code);
+                                insertStatement.setString(8, rateName);
+                                insertStatement.setString(9, rate);
+                                insertStatement.setString(10, tax);
+
+                                insertStatement.executeUpdate();
+                            }
+                        }
+
+                        connection.commit();
+                        System.out.println("Hotel database created");
+                    } catch (SQLException exc) {
+                        connection.rollback();
+                        exc.printStackTrace();
+                    } finally {
+                        connection.setAutoCommit(true);
+                    }
+                } catch (SQLException | NullPointerException exc) {
+                    exc.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 }
+
 
--- prediction-provider/src/main/java/org/example/control/WeatherController.java
+++ prediction-provider/src/main/java/org/example/control/WeatherController.java
@@ -14,7 +14,7 @@ public class WeatherController {
     }
 
     public void execute() {
-        Location madrid = new Location("Madrid", 40.4168, -3.7038);
+        Location madrid = new Location("Madrid", 40.41, -3.70);
         Location barcelona = new Location("Barcelona", 41.3888, 2.159);
         Location sevilla = new Location("Sevilla", 37.3886, -5.9822);
         Location valencia = new Location("Valencia", 39.4699, -0.3763);
